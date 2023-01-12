42a65a5e31042c6684c77880a09f3fb98ff202d8
Jedid95 <jedid.santos@gmail.com>
2023-01-12 23:47:01 +0000

    desafio - temperatura

--- java2022_completo_do_zero_ao_profissional_projetos/exercicios/src/fundamentos/Temperatura.java
+++ java2022_completo_do_zero_ao_profissional_projetos/exercicios/src/fundamentos/Temperatura.java
@@ -1,7 +1,21 @@
 package fundamentos;
 
+/*
+ * Desafio de calculo de temperatura
+ * (ºF - 32) x 5/9 = ºC
+ */
+
 public class Temperatura {
 	public static void main(String[] args) {
-		// (ºF - 32) x 5/9 = ºC
+		
+		//constantes
+		final double ajuste = 32;
+		final double fator = 5.0/9.0;
+		
+		//variaveis
+		double F = 32;
+		double C = (F-ajuste)*fator;
+		
+		System.out.println("Temperatura: " + F + "F Equivale a: " + C + "ºC");
 	}
 }
