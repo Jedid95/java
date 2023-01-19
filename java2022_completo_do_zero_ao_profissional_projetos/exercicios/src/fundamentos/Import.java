827b5a9a8731c80417b90562f1e73fa2530c3374
Jedid95 <jedid.santos@gmail.com>
2023-01-19 23:17:33 +0000

    import

---
+++ java2022_completo_do_zero_ao_profissional_projetos/exercicios/src/fundamentos/Import.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.util.Date;
+
+//import javax.swing.JButton;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		//Exemplo de código que não precisa de import
+		String s = "Bom dia";
+		System.out.println(s);
+		
+		//Exemplo de código que precisa de import
+		Date d = new Date();
+		System.out.println(d);
+		
+		
+		//JButton botao = new JButton();
+		
+	}
+}
