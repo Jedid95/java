5804d99397afdfd477f60aa2dae7766f6ba96395
Jedid95 <jedid.santos@gmail.com>
2023-01-23 22:56:33 +0000

    Tipo string

--- java2022_completo_do_zero_ao_profissional_projetos/exercicios/src/fundamentos/TipoString.java
+++ java2022_completo_do_zero_ao_profissional_projetos/exercicios/src/fundamentos/TipoString.java
@@ -6,8 +6,18 @@ public class TipoString {
 		System.out.println("Olá Pessoal".charAt(2));
 		
 		String s = "Boa tarde";
-		s = s.toUpperCase(s);
+		s = s.toUpperCase();
 		System.out.println(s.concat("!!!"));
 		System.out.println(s + "!!!");
+		System.out.println(s.startsWith("Boa"));
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.length());
+		System.out.println(s.equals("boa tarde"));
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+		var nome = "Pedro";
+		var sobrenome = "Santos";
+		
+		System.out.printf("Nome: %s %s", nome, sobrenome);
 	}
 }
